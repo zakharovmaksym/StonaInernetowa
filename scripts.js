648611d49a5ad2a87efaee02d5a44aea78280d10
zakharovmaksym <zakharovmaksym28@gmail.com>
2024-05-19 20:03:49 +0200

    Prosta strona internetowa z kursu SDA

---
+++ scripts.js
@@ -0,0 +1,13 @@
+console.log("Działam poprawnie")
+
+
+
+//const p = document.createElement("p")
+//console.log(p)
+
+const header=document.querySelector("header")
+console.log(header)
+
+const p = document.createElement("p")
+p.innerText="To nie jaest of strona Krakowa"
+header.appendChild(p)
